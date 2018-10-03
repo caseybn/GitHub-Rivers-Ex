9bf22fa0a376182a0ccbb9f65e66ae13c23370bc
Casey <bnc0015@mix.wvu.edu>
2018-10-02 21:14:03 -0400

    attempt at the code to download the USGS data, not quite right

--- streamgauge_info.sh
+++ streamgauge_info.sh
@@ -1,1 +1,8 @@
-### I am actually stuck just trying to find the proper URL to link wget to.
+# still trying to come up with the correct code
+#depending on how it downloads we might be able to just use cut to give the most recent record??
+
+for gaugue in "02109500" "02134500" "02091814" "02105769"
+  do
+  wget -O "$guage".txt https://nwis.waterdata.usgs.gov/nc/nwis/uv/?cb_00060=on&format=rdb&site_no="$gauge"&period=&begin_date=1900-09-24&end_date=2018-10-01
+  cut head -n 1 > "$gauge".text
+done
