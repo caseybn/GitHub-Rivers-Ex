749f57a290954cc62d48b0fabc5483ef99a2494a
Casey <bnc0015@mix.wvu.edu>
2018-10-04 11:13:50 -0400

    corrected spelling mistakes guage

--- streamgauge_info.sh
+++ streamgauge_info.sh
@@ -1,8 +1,8 @@
 # still trying to come up with the correct code
 #depending on how it downloads we might be able to just use cut to give the most recent record??
 
-for guage in "02109500" "02134500" "02091814" "02105769"
+for gauge in "02109500" "02134500" "02091814" "02105769"
   do
-  wget -O "$guage".txt "https://nwis.waterdata.usgs.gov/nc/nwis/uv/?cb_00060=on&format=rdb&site_no="$gauge"&period=&begin_date=1900-09-24&end_date=2018-10-01"
+  wget -O "$gauge".txt "https://nwis.waterdata.usgs.gov/nc/nwis/uv/?cb_00060=on&format=rdb&site_no="$gauge"&period=&begin_date=1900-09-24&end_date=2018-10-01"
   sed -n '17p' "$gauge".txt >> streamflows.txt
 done
