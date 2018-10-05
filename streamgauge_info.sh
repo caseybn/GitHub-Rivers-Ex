dbe15c0c2bba58845f7b9a01dce43621a520eedb
Casey <bnc0015@mix.wvu.edu>
2018-10-04 22:59:11 -0400

    corrected lines 9 and 10, this should work...?

--- streamgauge_info.sh
+++ streamgauge_info.sh
@@ -6,11 +6,11 @@
 for gauge in "02109500" "02134500" "02091814" "02105769"
   do
   wget -O "$gauge".txt "https://nwis.waterdata.usgs.gov/nc/nwis/uv/?cb_00060=on&format=rdb&site_no="$gauge"&period=&begin_date=1900-09-24&end_date=2018-10-01"
-  sed -n '17p' "$gauge".txt >> siteflow.txt
-  cut -f 3,5 "$gauge" | tail -n 1 >> streamflow.txt
+  sed -n '17p' "$gauge".txt >> streamflow.txt
+  cut -f 3,5 "$gauge".txt | tail -n 1 >> streamflow.txt
 done
 
 #paste information into a neat line per site
 paste - - - < streamflow.txt > currentflow.txt
 #removes old text files
-rm siteflow.txt streamflow.txt
+rm streamflow.txt
